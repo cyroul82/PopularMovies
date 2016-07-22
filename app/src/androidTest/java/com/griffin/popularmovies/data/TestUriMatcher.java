404f12d689a798c141e77751711cb83b0fdd277d
Cyril RAT <cyril.rat@gmail.com>
2016-07-22 21:02:46 +0200

    Problem Updating UI for Favorite Movies
    Within DetailFavoriteMovieFragment onLoadFinished.

---
+++ app/src/androidTest/java/com/griffin/popularmovies/data/TestUriMatcher.java
@@ -0,0 +1,27 @@
+package com.griffin.popularmovies.data;
+
+import android.content.UriMatcher;
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+/**
+ * Created by griffin on 16/07/16.
+ */
+public class TestUriMatcher extends AndroidTestCase {
+    private static final int MOVIE_ID = 2569;
+
+    // content://com.griffin.popularmovies/movie
+    private static final Uri TEST_MOVIE_DIR = MovieContract.FavoriteMoviesEntry.CONTENT_URI;
+    private static final Uri TEST_MOVIE_WITH_ID = MovieContract.FavoriteMoviesEntry.buildMovieUriFromIdMovie(MOVIE_ID);
+
+
+    public void testUriMatcher() {
+        UriMatcher testMatcher = MovieProvider.buildUriMatcher();
+
+        assertEquals("Error: The WEATHER URI was matched incorrectly.",
+                testMatcher.match(TEST_MOVIE_DIR), MovieProvider.MOVIE);
+        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
+                testMatcher.match(TEST_MOVIE_WITH_ID), MovieProvider.MOVIE_WITH_ID);
+
+    }
+}
